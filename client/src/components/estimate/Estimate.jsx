b3965feb56f13c09ad114c16756d2643142c887b
unknown <shamim.rahman@northsouth.edu>
2021-09-13 22:41:42 +0600

    updated

--- client/src/components/estimate/Estimate.jsx
+++ client/src/components/estimate/Estimate.jsx
@@ -6,24 +6,6 @@ const Estimate = () => {
       <h1>I am Estimate form</h1>
       <p>An estimate form will be shown here</p>
       <ul>
-        <li>A</li>
-        <li>B</li>
-        <li>B</li>
-        <li>B</li>
-        <li>B</li>
-
-        <li>B</li>
-
-        <li>B</li>
-        <li>B</li>
-        <li>B</li>
-        <li>B</li>
-        <li>B</li>
-        <li>B</li>
-        <li>B</li>
-        <li>B</li>
-        <li>B</li>
-
         <input placeholder="name" type="text"></input>
         <input placeholder="name" type="text"></input>
       </ul>
